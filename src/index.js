0e599265bb08de4073a92071d1ce97544800d53e
Ivanna86Koval <ms.ivanna86@gmail.com>
2023-07-22 19:08:27 +0300

    update loading

--- src/index.js
+++ src/index.js
@@ -1,12 +1,67 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 import SlimSelect from 'slim-select';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import Notiflix from 'notiflix';
 import 'slim-select/dist/slimselect.css';
 
-const selectorsSearch = {
-  select: document.querySelector('.breed-select'),
-  tagOption: document.querySelector('option'),
+const refs = {
+  selected: document.querySelector('.breed-select'),
   loader: document.querySelector('.loader'),
   error: document.querySelector('.error'),
   div: document.querySelector('.cat-info'),
+  option: document.querySelector('.breed-select option'),
 };
+refs.selected.classList.add('hidden');
+refs.option.setAttribute('selected', 'selected');
+refs.option.setAttribute('disabled', 'disabled');
+refs.option.textContent = 'Please selected breeds cat';
+refs.selected.setAttribute('id', 'slim-select');
+refs.selected.addEventListener('change', onSelect);
+
+function onSelect(event) {
+  refs.loader.classList.remove('hidden');
+  refs.div.innerHTML = '';
+  fetchCatByBreed(event.target.value)
+    .then(response => {
+      makeMarkCard(response.data[0].breeds[0], response.data[0]);
+      refs.loader.classList.add('hidden');
+    })
+    .catch(error => {
+      Notiflix.Notify.failure('Error getting data. Please try again later.');
+      refs.loader.classList.add('hidden');
+    })
+    .finally();
+}
+
+fetchBreeds()
+  .then(response => {
+    makeMarkOptions(response.data);
+    new SlimSelect({
+      select: '#slim-select',
+    });
+    refs.selected.classList.remove('hidden');
+    refs.loader.classList.add('hidden');
+  })
+  .catch(error => {
+    Notiflix.Notify.failure('Error getting data. Please try again later.');
+    refs.loader.classList.add('hidden');
+  })
+  .finally();
+
+function makeMarkOptions(arr) {
+  return arr.forEach(({ name, id }) => {
+    let markUp = `<option value="${id}">${name}</option>`;
+    refs.selected.insertAdjacentHTML('beforeend', markUp);
+  });
+}
+
+function makeMarkCard({ name, description, temperament }, { url }) {
+  let markUp = `
+    <img src="${url}" alt="${name}" width="460px"/>
+    <div class="descrp-card">
+    <p class="title-card">${name}</p>
+    <p class="description">${description}</p>
+    <p class="temerament">${temperament}</p>
+    </div>`;
+
+  refs.div.innerHTML = markUp;
+}

856f7bde93a4eb35fb8102105fb3b45735c24d1b
Ivanna86Koval <ms.ivanna86@gmail.com>
2023-07-22 17:24:51 +0300

    Update cat-api.js

--- src/cat-api.js
+++ src/cat-api.js
@@ -6,3 +6,11 @@ axios.defaults.headers.common['x-api-key'] =
 const BASE_URL = 'https://api.thecatapi.com/v1/';
 const END-POINT_COLECTION = 'breeds';
 const END-POINT_SEARCH = 'images/search';
+
+export function fetchBreeds() {
+    return axios.get(`${BASE_URL}${END-POINT_COLECTION}`)
+};
+
+export function fetchCatByBreed(breedId) {
+    return axios.get(`${BASE_URL}${END-POINT_SEARCH}?breed_ids=${breedId}&has_breeds=1`)
+};

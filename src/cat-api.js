6d7ec36af9be37d55c210467e828108314ae9bba
Ivanna86Koval <ms.ivanna86@gmail.com>
2023-07-22 16:57:58 +0300

    summary

---
+++ src/cat-api.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+axios.defaults.headers.common['x-api-key'] =
+  'live_2sNEA951h1xOKEC3rDFFmguazH2sPWeHNcoKqYk3Z2cmiIipFxYy3X0NaufoiXUx';
+
+const BASE_URL = 'https://api.thecatapi.com/v1/';
+const END-POINT_COLECTION = 'breeds';
+const END-POINT_SEARCH = 'images/search';
